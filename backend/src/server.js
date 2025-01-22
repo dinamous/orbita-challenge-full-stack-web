f5613968dd26f0be66564bb9b113d9433220cbef
Matheus Simões <matheusaugustosimoes@hotmail.com>
2025-01-21 23:35:27 -0300

    feat: :rocket: Connecting the databse with backend application

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,23 @@
 import express from 'express'
 import useRoutes from './routes.js'
+import User from './models/Student.js'
+import config from './config/database.js'
+import { Sequelize } from 'sequelize'
 
 const app = express()
 app.use(express.json())
 
+const sequelize = new Sequelize(config)
+User.init(sequelize)
+
 app.use('/students', useRoutes)
-app.listen(3000, (err, res) => {
-  console.log("Server on")
+
+sequelize.authenticate().then(() => {
+  console.log("Database Connected")
+  app.listen(3000, (err, res) => {
+    console.log("Server on")
+  })
+}).catch((err) => {
+  console.error(err)
 })
+
