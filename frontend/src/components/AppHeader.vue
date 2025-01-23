bde9c27f52f6be1dbbdbb03998be654c1dcc0404
Matheus Simões <matheusaugustosimoes@hotmail.com>
2025-01-23 18:12:00 -0300

    feat: :sparkles: Adding action to profile component

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import { useAppStore } from '@/stores/app';
+
+const appStore = useAppStore();
+
+const toggleSidebar = () => {
+  appStore.toggleSidebar();
+};
+</script>
 <template>
   <v-app-bar>
     <v-app-bar-nav-icon @click="toggleSidebar" />
@@ -8,19 +17,25 @@
           <v-icon icon="mdi-bell-outline" />
         </v-badge>
       </v-btn>
-      <v-avatar>
-        <v-img src="https://dinamousdev.vercel.app/assets/avatar-transparente-9vTT8Jih.png" />
-      </v-avatar>
+
+      <v-menu>
+        <template #activator="{ props }">
+          <v-avatar v-bind="props">
+            <v-img cover src="https://dinamousdev.vercel.app/assets/avatar-transparente-9vTT8Jih.png" />
+          </v-avatar>
+        </template>
+
+        <v-card min-width="">
+          <v-list density="compact" :lines="false" nav>
+            <v-list-item prepend-icon="mdi-account-outline" to="">
+              <v-list-title>Meu Perfil</v-list-title>
+            </v-list-item>
+            <v-list-item prepend-icon="mdi-exit-to-app" to="">
+              <v-list-title>Sair</v-list-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
     </template>
   </v-app-bar>
 </template>
-
-<script setup lang="ts">
-import { useAppStore } from '@/stores/app';
-
-const appStore = useAppStore();
-
-const toggleSidebar = () => {
-  appStore.toggleSidebar();
-};
-</script>
