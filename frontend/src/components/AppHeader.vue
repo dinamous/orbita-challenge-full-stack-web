4db6a40055f842d33b255aee9e28b31e0d6d1590
Matheus Simões <matheusaugustosimoes@hotmail.com>
2025-01-23 16:31:00 -0300

    feat: :sparkles: Adding the Header of the page
    Adding the header with the hamburgue and profile

---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-nav-icon />
+    <v-app-bar-title>Meu App</v-app-bar-title>
+    <template #append>
+      <v-btn icon>
+        <v-badge info>
+          <v-icon icon="mdi-bell-outline" />
+        </v-badge>
+      </v-btn>
+      <v-avatar>
+        <v-img src="https://dinamousdev.vercel.app/assets/avatar-transparente-9vTT8Jih.png" />
+      </v-avatar>
+    </template>
+  </v-app-bar>
+</template>
--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <HelloWorld />
+  <AppHeader />
+
+  <v-main>
+    <v-container>
+      <h1>Dashboard</h1>
+      <v-row>
+        <v-col cols="12" md="4" lg="3">
+          <v-card>
+            <v-img
+              src="https://images.pexels.com/photos/5427651/pexels-photo-5427651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            TEtte
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
