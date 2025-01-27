ebb42c3eae41f3ad46081dc08e0251f13bd2f1e2
Matheus Simões <matheusaugustosimoes@hotmail.com>
2025-01-27 11:22:04 -0300

    feat: :sparkles: Dding github Actions
    Added a basic githubAction integration

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -7,10 +7,8 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AppFooter: typeof import('./components/AppFooter.vue')['default']
     AppHeader: typeof import('./components/AppHeader.vue')['default']
     ButtonAddStudent: typeof import('./components/ButtonAddStudent.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Sidebar: typeof import('./components/Sidebar.vue')['default']
